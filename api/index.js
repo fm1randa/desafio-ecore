2e4e1e696f06c54065e9fde9822978cc582039b6
Filipe Miranda <filipe.rj149@gmail.com>
2021-10-05 20:55:36 -0300

    added list and route to get list

--- api/index.js
+++ api/index.js
@@ -6,7 +6,16 @@ app.use(express.json());
 app.use(cors());
 const port = 3000;
 
+const list = [
+	{ id: 1, name: "Filipe Miranda", age: 20 },
+	{ id: 2, name: "Tiago Miranda", age: 11 },
+];
+
+let sequence = 3;
+
 app.get("/", (req, res) => res.send("API is running!"));
+
+app.get("/list", (req, res) => res.status(200).json(list));
 
 app.listen(3000, () => {
 	console.log(`api is running @ localhost:${port}`);
